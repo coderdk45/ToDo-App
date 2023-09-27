1725cb4c46fe645ab6ea54b341e621eea82be427
Deepak Prajapati <coderdk45@gmail.com>
2023-09-27 09:55:02 +0530

    todolist-created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import TodoList from './component/TodoList';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem('todos')) || [];
+    setTodos(storedTodos);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  const addTodo = (text) => {
+    setTodos([...todos, { text, id: Date.now() }]);
+  };
+
+  const removeTodo = (id) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <div className="card-header">ToDo App</div>
+      <div className="card-body">
+        <input
+          type="text"
+          placeholder="Add a new text"
+          onKeyPress={(e) => {
+            if (e.key === 'Enter') {
+              addTodo(e.target.value);
+              e.target.value = '';
+            }
+          }}
+        />
+      </div>
+      <TodoList todos={todos} removeTodo={removeTodo} />
     </div>
   );
 }
